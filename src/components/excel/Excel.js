b9fff78f7660fffabaea90460ef0964aac8771ce
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2020-10-17 10:34:26 +0300

    Added new methods to Dom class, rewrote existing code, added comments

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -2,21 +2,27 @@ import { $ } from '@core/dom';
 
 class Excel {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector);
-    this.components = options.components || [];
+    this.$el = $(selector); // main div with id #app
+    this.components = options.components || []; // array of Header, Table, Toolbar, Formula
   }
 
   getRoot() {
-    const $root = $.create('div', 'excel');
+    const $root = $.create('div', 'excel'); // creates main excel page div with class excel
     this.components.forEach((Component) => {
+      // creates root div for excel component (<div class="header"></>)
       const $el = $.create('div', Component.className);
+      // creates component html (<button></button>)
       const component = new Component($el);
-      $el.innerHTML = component.toHTML();
+      // append component html to root div (<div class="header"><button>click</button></div>)
+      $el.html(component.toHTML());
+      // append component (header with all inner html) to root div with class excell
       $root.append($el);
     });
+    // returns whole excel component
     return $root;
   }
 
+  // inserts excel component in div with class 'app'
   render() {
     this.$el.append(this.getRoot());
   }
--- src/core/dom.js
+++ src/core/dom.js
@@ -1,14 +1,50 @@
 class Dom {
+  constructor(selector) {
+    this.$el = typeof selector === 'string'
+      ? document.querySelector(selector) : selector;
+  }
+
+  html(html) {
+    if (typeof html === 'string') {
+      this.$el.innerHTML = html;
+      return this;
+    }
+    return this.$el.outerHTML.trim();
+  }
+
+  clear() {
+    this.html = '';
+    return this;
+  }
+
+  on() {}
+
+  append(node) {
+    let element = node;
+    if (element instanceof Dom) {
+      element = node.$el;
+    }
+    if (Element.prototype.append) {
+      this.$el.append(element);
+    } else {
+      this.$el.appendChild(element);
+    }
+    return this;
+  }
 }
 
-export function $() {
-  return new Dom();
+// Function to use in other files to create Dom class instances and
+// to call Dom class methods
+export function $(selector) {
+  return new Dom(selector);
 }
 
-$.create = (tagName, classes = ' ') => {
+$.create = (tagName, classes = '') => {
+  // creates new div with css class
   const el = document.createElement(tagName);
   if (classes) {
     el.classList.add(classes);
   }
-  return el;
+  // returns new instance of Dom class, now it can use append, clear & html methods
+  return $(el);
 };
