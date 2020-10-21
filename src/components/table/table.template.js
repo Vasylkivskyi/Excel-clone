8143eb2066b98d1f2c6a53329306b4feb3e52bf9
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2020-10-21 07:29:44 +0300

    add solution to create cells

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,11 +3,9 @@ const CODS = {
   Z: 90,
 };
 
-// function createCell() {
-//   return `
-//     <div class="cell" contenteditable>A1</div>
-//   `;
-// }
+function createCell() {
+  return '<div class="cell" contenteditable></div>';
+}
 
 function toColumn(column) {
   return `
@@ -17,10 +15,10 @@ function toColumn(column) {
   `;
 }
 
-function createRow(content) {
+function createRow(content, index) {
   return `
     <div class="row">
-      <div class="row-info"></div>
+      <div class="row-info">${index || ''}</div>
       <div class="row-data">${content}</div>
     </div>
   `;
@@ -39,9 +37,14 @@ export function createTable() {
     .map(toColumn)
     .join('');
 
+  const cells = new Array(colsCount)
+    .fill()
+    .map(createCell)
+    .join('');
+
   rows.push(createRow(cols));
   for (let i = 0; i < colsCount; i++) {
-    rows.push(createRow());
+    rows.push(createRow(cells, i + 1));
   }
 
   return rows.join('');
