8bf502a5c9ae3cdbf8e8e027fbe272b105d16e6c
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2020-10-25 20:03:46 +0200

    add basic listeners to table component

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,6 +4,28 @@ import { createTable } from './table.template';
 class Table extends ExcelComponent {
   static className = 'excel__table';
 
+  constructor($root) {
+    super($root, {
+      listeners: ['click', 'mousedown', 'mousemove', 'mouseup'],
+    });
+  }
+
+  onClick() {
+    console.log('click');
+  }
+
+  onMousedown() {
+    console.log('mousedown');
+  }
+
+  onMousemove() {
+    console.log('mousemove');
+  }
+
+  onMouseup() {
+    console.log('mouseup');
+  }
+
   toHTML() {
     return createTable(25);
   }
