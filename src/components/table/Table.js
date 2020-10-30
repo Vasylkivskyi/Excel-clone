cfc078af7527771b7fa40142b29ccbab07270ed2
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2020-10-30 08:00:01 +0200

    made cell resize optimization

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -19,17 +19,14 @@ class Table extends ExcelComponent {
     if (event.target.dataset.resize) {
       const $resizer = $(event.target);
       const $parent = $resizer.closest('[data-type="resizable"]');
-      console.log($parent.data);
       const coordinates = $parent.getCoordinates();
+      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`);
       document.onmousemove = (e) => {
         const delta = e.pageX - coordinates.right;
         const value = coordinates.width + delta;
         $parent.$el.style.width = `${value}px`;
-
-        // TODO need to make optimization
-        document.querySelectorAll(`[data-col="${$parent.data.col}"]`)
-          // eslint-disable-next-line no-param-reassign
-          .forEach((el) => { el.style.width = `${value}px`; });
+        // eslint-disable-next-line no-param-reassign
+        cells.forEach((el) => { el.style.width = `${value}px`; });
       };
       document.onmouseup = () => {
         document.onmousemove = null;
